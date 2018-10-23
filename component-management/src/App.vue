44098683134e962a52618192c324d25761f2dc46
Lương Kiều Thiên <41820342+ThienLuong95@users.noreply.github.com>
2018-10-23 18:35:54 +0700

    update mock project

--- component-management/src/App.vue
+++ component-management/src/App.vue
@@ -1,91 +1,154 @@
 <template>
-  <v-app>
-    <v-navigation-drawer
-      persistent
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      enable-resize-watcher
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile
-          value="true"
-          v-for="(item, i) in items"
-          :key="i"
+    <v-app>
+        <!--Navigation-->
+        <v-navigation-drawer
+                persistent
+                v-model="drawer"
+                fixed
+                app
         >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar
-      app
-      :clipped-left="clipped"
-    >
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
-    </v-toolbar>
-    <v-content>
-      <router-view/>
-    </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile @click="right = !right">
-          <v-list-tile-action>
-            <v-icon>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
-    </v-footer>
-  </v-app>
+            <v-select
+                    :items="projects"
+                    item-text="projectName"
+                    item-value="projectName"
+                    v-model="selectedProject"
+                    return-object
+                    label="Selected"
+                    v-on:change="onProjectSelection"
+                    style="padding: 24px 16px 0 16px"
+                    outline
+
+            ></v-select>
+
+            <!--Search box search component-->
+            <v-text-field
+                    v-model="keyword"
+                    append-icon="search"
+                    label="Search"
+                    single-line
+                    hide-details
+                    v-on:change="onSearch(keyword)"
+                    v-on:blur="onSearch(keyword)"
+                    style="padding: 0 16px"
+                    outline
+            ></v-text-field>
+
+            <!--Show list Component after search, default return all list-->
+            <v-list>
+                <v-list-tile
+                        v-for="(item) in listResult"
+                        :key="item.id"
+                        :to="{ name: 'component', params: { id: item.id, data: item }}"
+                        active-class="primary--text"
+                        @click="onComponentSelected"
+                >
+                    <v-list-tile-content style="padding: 0 8px">
+                        <v-list-tile-title>{{item.name}}</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+
+        </v-navigation-drawer>
+
+        <!--Toolbar-->
+        <v-toolbar
+                dark
+                app
+
+        >
+            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-title v-text="title"></v-toolbar-title>
+            <v-spacer></v-spacer>
+            <!--Toolbar items-->
+            <v-toolbar-items style="margin: 0 24px;">
+                <v-menu offset-y>
+                    <v-btn
+                            large
+                            style="border-radius: 0;border-width: 0 1px; border-style: solid; border-color: darkgrey; font-weight: 500!important;; font-size: 18px !important;"
+                            flat
+                            slot="activator"
+                    >
+                        <v-icon medium left dark>add</v-icon>
+                        Add
+                        <v-icon medium right dark>arrow_drop_down</v-icon>
+                    </v-btn>
+                    <v-list>
+                        <v-list-tile
+                                v-for="(item, index) in menuItems"
+                                :key="index"
+                                @click=""
+                        >
+                            <v-list-tile-title>{{ item }}</v-list-tile-title>
+                        </v-list-tile>
+                    </v-list>
+                </v-menu>
+
+            </v-toolbar-items>
+
+            <!--Avatar-->
+            <v-avatar v-on:click="rightDrawer = !rightDrawer" style=" cursor: pointer "
+                      size='40px'
+            >
+                <img src="/static/asset/avatar.svg" alt="avatar">
+            </v-avatar>
+        </v-toolbar>
+
+        <!--Content-->
+        <v-content>
+            <router-view/>
+        </v-content>
+        <v-navigation-drawer
+                temporary
+                :right="right"
+                v-model="rightDrawer"
+                fixed
+                app
+        >
+            User profile coming soon
+        </v-navigation-drawer>
+
+        <!--Footer-->
+        <v-footer >
+            <span>&copy; ATM</span>
+        </v-footer>
+    </v-app>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      clipped: false,
-      drawer: true,
-      fixed: false,
-      items: [{
-        icon: 'bubble_chart',
-        title: 'Inspire'
-      }],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+    export default {
+        data() {
+            return {
+                menuItems: ['New Project', 'New Component', 'New User'],
+                drawer: true,
+                projects: [],
+                keyword: '',
+                selectedProject: null,
+                listResult: [],
+                items: [{
+                    icon: 'bubble_chart',
+                    title: 'Inspire'
+                }],
+                right: true,
+                rightDrawer: false,
+                title: 'Component Management'
+            }
+        },
+        name: 'App',
+        methods: {
+            onProjectSelection: function () {
+
+            },
+            onSearch(){
+
+            },
+            onComponentSelected(){
+
+            }
+        }
     }
-  },
-  name: 'App'
-}
 </script>
+<style scoped>
+    .v-menu__content {
+        border-radius: 0 !important;
+    }
+</style>
--- component-management/src/main.js
+++ component-management/src/main.js
@@ -3,22 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import {
-  Vuetify,
-  VApp,
-  VNavigationDrawer,
-  VFooter,
-  VList,
-  VBtn,
-  VIcon,
-  VGrid,
-  VToolbar,
-  transitions
-} from 'vuetify'
-import '../node_modules/vuetify/src/stylus/app.styl'
 
-Vue.use(Vuetify, {
-  components: {
+//Event bus
+const EventBus = new Vue();
+Vue.prototype.$eventBus = EventBus;
+
+import {
+    Vuetify,
     VApp,
     VNavigationDrawer,
     VFooter,
@@ -27,25 +18,47 @@ Vue.use(Vuetify, {
     VIcon,
     VGrid,
     VToolbar,
+    VAvatar,
+    VMenu,
+    VSelect,
+    VTextField,
     transitions
-  },
-  theme: {
-    primary: '#ee44aa',
-    secondary: '#424242',
-    accent: '#82B1FF',
-    error: '#FF5252',
-    info: '#2196F3',
-    success: '#4CAF50',
-    warning: '#FFC107'
-  }
-})
+} from 'vuetify'
+import '../node_modules/vuetify/src/stylus/app.styl'
 
-Vue.config.productionTip = false
+Vue.use(Vuetify, {
+    components: {
+        VApp,
+        VNavigationDrawer,
+        VFooter,
+        VList,
+        VBtn,
+        VIcon,
+        VGrid,
+        VToolbar,
+        VAvatar,
+        VMenu,
+        VSelect,
+        VTextField,
+        transitions
+    },
+    theme: {
+        primary: '#ee44aa',
+        secondary: '#424242',
+        accent: '#82B1FF',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FFC107'
+    }
+});
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+    el: '#app',
+    router,
+    components: {App},
+    template: '<App/>'
+});
